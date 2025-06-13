2b7859f5df164f57598ef05af9025c9eaa0105f2
adwaykanhere <adway.kanhere@gmail.com>
2025-06-13 17:25:05 +0000

    Deploying to gh-pages from @ adwaykanhere/adwaykanhere.github.io@b790958c6750934b32a2bd3f05921f14f92d937a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "An extended collection of my projects and media.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
